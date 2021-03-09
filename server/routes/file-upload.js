2d6e85027be3efc34c59b0d2c6bce3b32a2d9f83
Cesar Martinez <chernanma@msn.com>
2021-03-08 22:18:53 -0500

    Set Up the Image Upload Route, to store images in S3 bucket

--- server/routes/file-upload.js
+++ server/routes/file-upload.js
@@ -5,11 +5,11 @@ const upload = require('../services/file-upload');
 
 const singleUpload = upload.single('image');
 
-router.post('/image-upload', (req, res) => {
-    singleUpload(req, res, err => {
-        return res.json({'imageUrl' : req.file.location});
-    });
-});
+// router.post('/image-upload', (req, res) => {
+//     singleUpload(req, res, err => {
+//         return res.json({'imageUrl' : req.file.location});
+//     });
+// });
 
 // router.get('/users', (req, res) => {
 //     res.json({"which": "which"})
